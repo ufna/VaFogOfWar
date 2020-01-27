a0a55db86a9fba0995f05a057607db427c49961f
Vladimir Alyamkin <ufna@ufna.ru>
2020-01-27 12:39:16 +0300

    Update clang-format

--- Source/VaFogOfWar/Private/VaFogDefines.h
+++ Source/VaFogOfWar/Private/VaFogDefines.h
@@ -11,6 +11,6 @@ DECLARE_STATS_GROUP(TEXT("VA Fog of War"), STATGROUP_VaFog, STATCAT_Advanced);
 
 DECLARE_LOG_CATEGORY_EXTERN(LogVaFog, Log, All);
 
-#define VA_FUNC (FString(__FUNCTION__))				 // Current Class Name + Function Name where this is called
-#define VA_LINE (FString::FromInt(__LINE__))		 // Current Line Number in the code where this is called
+#define VA_FUNC (FString(__FUNCTION__))              // Current Class Name + Function Name where this is called
+#define VA_LINE (FString::FromInt(__LINE__))         // Current Line Number in the code where this is called
 #define VA_FUNC_LINE (VA_FUNC + "(" + VA_LINE + ")") // Current Class and Line Number where this is called!
